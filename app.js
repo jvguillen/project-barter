67e2b278d466ba08057c17ab23f161be4b064d95
Jose Guillen <>
2017-08-19 15:45:50 -0400

    Implement helmet with express

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+"user strict";
+
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const helment = require('helment')
 
 const index = require('./routes/index');
 const users = require('./routes/users');
@@ -21,6 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
 
 app.use('/', index);
 app.use('/users', users);
