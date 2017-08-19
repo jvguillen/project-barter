fdfe61cf6527eeddbfbfdac36d8097120e90497e
Jose Guillen <>
2017-08-19 18:11:45 -0400

    Test json response

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const db = req.db;
+  const collection = db.get('users');
+
+  collection.find({},{},function(err, users){
+      res.status(200).json(users)
+  });
 });
 
 module.exports = router;
