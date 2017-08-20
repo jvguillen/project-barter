6fec0c174f111549c9cfbaa34c62ec06348f639c
Jose Guillen <>
2017-08-19 22:50:34 -0400

    Implement response object in /users route

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const response = require('../lib/response');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   const db = req.db;
   const collection = db.get('users');
 
-  collection.find({},{},function(err, users){
-      res.status(200).json(users)
+  collection.find({}, {}, function(err, users){
+
+    res.status(200).json(response(res, users));
   });
 });
 
