e6d136356494fa9a5e307c058f8af7811eb636ca
Jose Guillen <>
2017-08-19 22:50:13 -0400

    Implement response object in / route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const response = require('../lib/response');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+   res.status(200).json(response(res, "Connected!"));
 });
 
 module.exports = router;
