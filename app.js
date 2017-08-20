5a2219a09084dc3af5040e3878d4e9d743e9a76f
Jose Guillen <>
2017-08-20 18:48:35 -0400

    Remove serve-favicon module

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 const config = require('./config');
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
