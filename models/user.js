cf90876b811d6773268885e2d256724c8d203e1f
Jose Guillen <>
2017-08-20 16:55:38 -0400

    Change model & collection name to singular form

---
+++ models/user.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    username: String,
+    password: String,
+    age: Number,
+    email: {type: String, required: true},
+    active: Boolean,
+    updated: { type: Date, default: Date.now }
+}, { collection: 'user' });
+
+const userModel = mongoose.model('User', userSchema);
+
+module.exports = userModel;
