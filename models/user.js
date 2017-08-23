c93e4b9830c5a1c4a828abfbd6666c81078b0d8d
Wilhelm <wilhelm.siso@gmail.com>
2017-08-23 00:30:43 -0300

    oAuth authentication develop
    First try with Google oAuth
    
    New:
    - package.json updated
    - passport configuration
    - new config file

--- models/user.js
+++ models/user.js
@@ -7,13 +7,19 @@ const userSchema = new Schema({
   firstname: {type: String, required: false, min: [3, 'Supply at least 3 characters'] },
   lastname: {type: String, required: false, min: [3, 'Supply at least 3 characters'] },
   age: {type: Number, required: false},
-  email: { type: String, required: true, validate: {
+  email: { type: String, required: false, validate: {
       validator: (v) => {
         return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
       },
       message: '{VALUE} is not a valid email!'
     },
   },
+  google: {
+    id: {type: String},
+    token: {type: String},
+    email: {type: String},
+    name: {type: String},
+  }
   active: { type: Boolean, required: false, default: true },
   created: { type: Date, default: Date.now },
   updated: { type: Date, default: Date.now },
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ const router = express.Router();
 /* GET users listing. */
 router.get('/', (req, res) => User.find().then(users => res.status(200).json(response(res, users))));
 
+// Google routes
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/auth/google/callback', passport.authenticate('google', {
+  successRedirect: '/profile',
+  failureRedirect: '/',
+}));
+
 module.exports = router;
