b7cf88e36ac45a4931122902ebeec54bb70c310e
Jose Guillen <>
2017-08-26 13:43:58 -0400

    Add default auth config

--- config/default.js
+++ config/default.js
@@ -6,5 +6,12 @@ module.exports = {
     name: process.env.DB_NAME,
     port: process.env.DB_PORT,
   },
+  auth: {
+    googleAuth: {
+      clientID: process.env.AUTH_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      callbackURL: process.env.AUTH_GOOGLE_CALLBACK,
+    },
+  },
   logging: false,
 };
