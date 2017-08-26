dcd10c4ea56b34c5a2084c937b5aaf1d13e65130
Jose Guillen <>
2017-08-26 13:44:08 -0400

    Add dev auth config

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,12 @@ module.exports = {
     name: 'bartoapp_dev',
     port: '27017',
   },
+  auth: {
+    google: {
+      clientID: '503925882421-6lkiaqssq9vunuene9fdvh7kbf3mdu3e.apps.googleusercontent.com',
+      clientSecret: '703eIC3RVfItUn9ykE37H42b',
+      callbackURL: 'http://localhost:3000/auth/google/callback',
+    },
+  },
   logging: true,
 };
