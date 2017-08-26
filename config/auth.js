54c97f3e90954aca3dcbd3e63d36118d11cf8b91
Wilhelm <wilhelm.siso@gmail.com>
2017-08-26 13:26:52 -0300

    Google OAuth wroking
    Problem solved with Google OAuth using passport package

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
 	googleAuth: {
 		clientID: '503925882421-6lkiaqssq9vunuene9fdvh7kbf3mdu3e.apps.googleusercontent.com',
-		secret: '703eIC3RVfItUn9ykE37H42b',
+		clientSecret: '703eIC3RVfItUn9ykE37H42b',
 		callbackURL: 'http://localhost:3000/auth/google/callback'
 	}
 }
