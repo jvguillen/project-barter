9f50bc533b7e6b328dcd5ef54d24731dd931a809
Jose Guillen <>
2017-08-27 10:43:38 -0400

    Add facebook oauth credentials for development

--- config/dev.js
+++ config/dev.js
@@ -10,6 +10,11 @@ module.exports = {
       clientSecret: '703eIC3RVfItUn9ykE37H42b',
       callbackURL: 'http://localhost:3000/auth/google/callback',
     },
+    facebook: {
+      clientID: '339206526491099',
+      clientSecret: '41a27fa7bfe36de4f4ce618ecd13a768',
+      callbackURL: 'http://localhost:3000/api/1/auth/facebook/callback',
+    },
   },
   logging: true,
 };
