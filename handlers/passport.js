c9ec43775878c1bad165aab31f0eec29147efb0f
Jose Guillen <>
2017-08-31 17:55:38 -0400

    Implement mongoose promises and arrow functions

--- handlers/passport.js
+++ handlers/passport.js
@@ -7,7 +7,7 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 const User = require('../models/user');
-const config = require('../config');
+const auth = require('../lib/auth');
 
 module.exports = (passport) => {
   passport.serializeUser((user, done) => { done(null, user.id); });
@@ -16,54 +16,29 @@ module.exports = (passport) => {
     User.findById(id, (err, user) => { done(err, user); });
   });
 
-  passport.use(new GoogleStrategy({
-    clientID: config.auth.google.clientID,
-    clientSecret: config.auth.google.clientSecret,
-    callbackURL: config.auth.google.callbackURL,
-  }, (token, refreshToken, profile, done) => {
+  passport.use(new GoogleStrategy(auth.google, (token, refreshToken, profile, done) => {
     process.nextTick(() => {
-      User.findOne({ 'google.id': profile.id }, (err, user) => {
-        if (err) return done(err); // throw err
+      User.findOne({ 'google.id': profile.id }).then((user) => {
         if (!user) {
           const newUser = new User(); // create a new user
-          newUser.google.id = profile.id;
-          newUser.google.token = token;
-          newUser.google.name = profile.displayName;
-          newUser.google.email = (profile.emails[0].value || '').toLowerCase();
-          newUser.save((err) => {
-            if (err) throw err;
-            return done(null, newUser);
-          });
+          newUser.google = auth.google.setProfile(profile, token);
+          return newUser.save().then(doc => done(null, doc)).catch((err) => { throw err; });
         }
-
         return done(null, user); // return user founds
-      });
+      }).catch(err => done(err));
     });
   }));
 
-  passport.use(new FacebookStrategy({
-    clientID: config.auth.facebook.clientID,
-    clientSecret: config.auth.facebook.clientSecret,
-    callbackURL: config.auth.facebook.callbackURL,
-    profileFields: ['id', 'email', 'first_name', 'last_name'],
-  }, (token, refreshToken, profile, done) => {
+  passport.use(new FacebookStrategy(auth.facebook, (token, refreshToken, profile, done) => {
     process.nextTick(() => {
-      User.findOne({ 'facebook.id': profile.id }, (err, user) => {
-        if (err) return done(err);
+      User.findOne({ 'facebook.id': profile.id }).then((user) => {
         if (!user) {
           const newUser = new User();
-          newUser.facebook.id = profile.id;
-          newUser.facebook.token = token;
-          newUser.facebook.name = `${profile.name.givenName}  ${profile.name.familyName}`;
-          newUser.facebook.email = (profile.emails[0].value || '').toLowerCase();
-          newUser.save((err) => {
-            if (err) throw err;
-            return done(null, newUser);
-          });
+          newUser.facebook = auth.facebook.setProfile(profile, token);
+          return newUser.save().then(doc => done(null, doc)).catch((err) => { throw err; });
         }
-
         return done(null, user);
-      });
+      }).catch(err => done(err));
     });
   }));
 };
