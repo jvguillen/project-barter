e227d41c2d30586e0ce9d555168d88eb7079078f
Jose Guillen <>
2017-09-24 21:58:50 -0400

    Get user's items endpoint

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,9 @@ router.get('/', (req, res) => User.find().then(users => res.status(200).json(res
 /* GET user profile page. */
 router.get('/profile', (req, res) => res.status(200).json(response(res, req.user._id)));
 
+/* GET  items of a specific user */
+router.get('/users/:id/items', (req, res) => {
+	Item.find({ '_user': req.params.id }).then(item => res.status(200).json(response(res, item)));
+});
+
 module.exports = router;
