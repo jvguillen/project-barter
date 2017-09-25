38eb8834753bf2e174052335a8d860f47eba3793
Jose Guillen <>
2017-09-24 21:59:38 -0400

    Refactor images endpoints

--- routes/images.js
+++ routes/images.js
@@ -5,20 +5,43 @@ const Image = require('../models/image');
 
 const router = express.Router({ mergeParams: true });
 
-/* GET items listing. */
+/* GET images listing. */
 router.get('/', (req, res) => {
 	Image.find()
 		.populate('_item')
 		.then(items => res.status(200).json(response(res, items)));
 });
 
-router.get('/image/:id', (req, res) => {
+/* GET an image by id */
+router.get('/images/:id', (req, res) => {
 	Image.findById(req.params.id)
 		.populate('_item')
 		.then(image => res.status(200).json(response(res, image)));
 });
 
-router.post('/image/:id/update', (req, res) => {
+/* POST create a new image */
+router.post('/images', multer({ dest: 'uploads/' }).single('image'), (req, res) => {
+
+	// validate input fields
+	req.checkBody('name', 'Name is required').notEmpty();
+	req.checkBody('path', 'File path is required').notEmpty();
+	req.checkBody('width', 'Image width is required').notEmpty();
+	req.checkBody('height', 'Image height is required').notEmpty();
+	req.checkBody('featured', 'Featured field is required').notEmpty();
+
+	const err = req.validationErrors();
+
+	if(err) { return res.status(200).json(response(res, { errors: err })); }
+
+	else {
+
+		const image = new Image({ name: req.file.originalname, path: req.body.path, width: req.body.width, height: req.body.height, featured: req.body.featured, _item: req.body.item });
+		image.save().then(err => res.status(200).json(response(res, { image: image, err: err })));
+	}
+});
+
+/* PUT Update an image */
+router.put('/images/:id', (req, res) => {
 	// Sanitize id passed in.
 	req.sanitize('id').escape();
 	req.sanitize('id').trim();
@@ -44,7 +67,8 @@ router.post('/image/:id/update', (req, res) => {
 	}
 });
 
-router.post('/image/:id/delete', (req, res) => {
+/* DELETE an image */
+router.delete('/images/:id', (req, res) => {
 	// Sanitize id passed in.
 	req.sanitize('id').escape();
 	req.sanitize('id').trim();
@@ -53,30 +77,6 @@ router.post('/image/:id/delete', (req, res) => {
 
 	Image.findByIdAndUpdate(req.params.id, item, {}).then(err => res.status(200).json(response(res, { item: item, err: err })));
 
-});
-
-router.get('/item/:id', (req, res) => {
-	Image.find({ '_item': req.params.id }).then(images => res.status(200).json(response(res, images)));
-});
-
-router.post('/new', multer({ dest: 'uploads/' }).single('image'), (req, res) => {
-
-	// validate input fields
-	req.checkBody('name', 'Name is required').notEmpty();
-	req.checkBody('path', 'File path is required').notEmpty();
-	req.checkBody('width', 'Image width is required').notEmpty();
-	req.checkBody('height', 'Image height is required').notEmpty();
-	req.checkBody('featured', 'Featured field is required').notEmpty();
-
-	const err = req.validationErrors();
-
-	if(err) { return res.status(200).json(response(res, { errors: err })); }
-
-	else {
-
-		const image = new Image({ name: req.file.originalname, path: req.body.path, width: req.body.width, height: req.body.height, featured: req.body.featured, _item: req.body.item });
-		image.save().then(err => res.status(200).json(response(res, { image: image, err: err })));
-	}
 });
 
 module.exports = router;
