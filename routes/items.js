794ebf81c7f4f6eb488709aaf86a9461a4cefd4b
Jose Guillen <>
2017-09-28 23:10:30 -0400

    Refactor item endpoints

--- routes/items.js
+++ routes/items.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const response = require('../lib/response');
 const Item = require('../models/item');
+const Image = require('../models/image');
 
 const router = express.Router({ mergeParams: true });
 
@@ -12,71 +13,70 @@ router.get('/', (req, res) => {
 });
 
 /* GET an item by id */
-router.get('/items/:id', (req, res) => {
+router.get('/:id', (req, res) => {
 	Item.findById(req.params.id)
 		.populate('_user')
 		.then(item => res.status(200).json(response(res, item)));
 });
 
 /* GET an item's images */
-router.get('/item/:id/images', (req, res) => {
+router.get('/:id/images', (req, res) => {
 	Image.find({ '_item': req.params.id }).then(images => res.status(200).json(response(res, images)));
 });
 
 /* POST Create a new item */
-router.post('/items', (req, res) => {
+router.post('/', (req, res) => {
 
 	// validate input fields
+	// this needs to return appropriate error code
 	req.checkBody('name', 'The name is required').notEmpty();
   req.checkBody('quality', 'You must supply the quality of the item').notEmpty();
 
 	const err = req.validationErrors();
 
-	if(err) { return res.status(200).json(response(res, { errors: err })); }
+	if(err) { return res.status(400).json(response(res, err, 'Bad Request')); }
 
 	else {
-
-		const item = new Item({ name: req.body.name, quality: req.body.quality, _user: req.body.user });
-		item.save().then(err => res.status(200).json(response(res, { item: item, err: err })));
+		const newItem = {
+			name: req.body.name,
+			quality: req.body.quality,
+			_user: req.body.user ,
+			active: true
+		};
+		const item = new Item(newitem);
+		item.save().then(item => res.status(200).json(response(res, item)));
 	}
 });
 
 /* PUT Update an item */
-router.put('/items/:id', (req, res) => {
+router.put('/:id', (req, res) => {
 	// Sanitize id passed in.
 	req.sanitize('id').escape();
 	req.sanitize('id').trim();
-
 	req.sanitize('name').escape();
 	req.sanitize('quality').escape();
 
 	// validate input fields
-	req.checkBody('name', 'The name is required').notEmpty();
-  req.checkBody('quality', 'You must supply the quality of the item').notEmpty();
-	req.checkBody('active', 'You must supply the status of the item').notEmpty();
+	// TODO: identify what is required
 
 	const err = req.validationErrors();
 
-	if(err) { return res.status(200).json(response(res, { err: err })) }
+	if(err) { return res.status(400).json(response(res, err, 'Bad Request')) }
 
 	else {
-
-		const item = new Item({ name: req.body.name, quality: req.body.quality, active: req.body.active });
-
-		Item.findByIdAndUpdate(req.params.id, item, {}).then(err => res.status(200).json(response(res, { item: item, err: err })));
+		//TODO: find a way to return the updated item instead of the old one.
+		Item.findByIdAndUpdate(req.params.id, req.body).then(item => res.status(200).json(response(res, item)));
 
 	}
 });
 
 /* DELETE remove an item by id */
-router.delete('/items/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
 	// Sanitize id passed in.
 	req.sanitize('id').escape();
 	req.sanitize('id').trim();
 
-	const item = new Item({ deleted: true });
-
-	Item.findByIdAndUpdate(req.params.id, item, {}).then(err => res.status(200).json(response(res, { item: item, err: err })));
+	Item.findByIdAndUpdate(req.params.id, { active: false, deleted: true }).then(item => res.status(200).json(response(res)));
 
 });
 
